0bf90e1249ece2b73005d562ceb23e33910a0de4
hyup <rainyseaside@naver.com>
2018-05-25 18:10:45 +0900

    "대강 종료"

--- app/src/main/java/com/hyupb/letswalk/Report.java
+++ app/src/main/java/com/hyupb/letswalk/Report.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -95,7 +96,8 @@ public class Report extends Fragment {
         }
 
         monthChart.setLineChartData(data2);
-
+        Log.i("aaa", "aaa");
+        Log.i("aaa", "aaa");
         return view;
     }
 
